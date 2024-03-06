f5b3112c6f88e6ea7725a69c87da65f607d2f90a
YIHONG <akashidaiki2@gmail.com>
2024-03-06 16:34:21 +0800

    替換母版內的內容

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,34 @@
 <!-- resources/views/layouts/app.blade.php -->
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <title>03_06 - @yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- 載入自訂的 css 檔案，寫法一 -->
+    <!-- <link rel="stylesheet" href="http://final.com/css/style.css"> -->
+
+    <!-- 載入自訂的 css 檔案，寫法二 -->
+    <!-- <link rel="stylesheet" href="{{asset("css/style.css")}}"> -->
 </head>
 
 <body>
-    This is the master sidebar.
-    <!-- @section('sidebar')
-    @show -->
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('dogs.index')}}">Dog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('cats.index')}}">Cat</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="container">
         @yield('content')
